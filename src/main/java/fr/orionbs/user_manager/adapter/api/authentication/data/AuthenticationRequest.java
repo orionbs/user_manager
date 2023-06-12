4382d1a0bec5def01d7b32770ccb7125f4bf5682
OrionBS <obeaunysugot@gmail.com>
2023-06-12 08:08:33 +0200

    updating authentication request.

--- src/main/java/fr/orionbs/user_manager/adapter/api/authentication/data/AuthenticationRequest.java
+++ src/main/java/fr/orionbs/user_manager/adapter/api/authentication/data/AuthenticationRequest.java
@@ -1,8 +1,10 @@
 package fr.orionbs.user_manager.adapter.api.authentication.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AuthenticationRequest {
     private String email;
     private String password;
